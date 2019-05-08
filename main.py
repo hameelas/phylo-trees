23a6a45003d3a36a1637b2352170bec7c5dd2640
Hamed Saleh <hameelas@gmail.com>
2019-05-08 17:52:52 -0400

    Testing stuff

--- main.py
+++ main.py
@@ -1,91 +1,99 @@
+import sys
+import test
+import math
+
 print("Hello")
 
 INF = float('Inf')
 
 def main(fname):
-    d = get_distances(fname)
-    n = len(d)
+	d = get_distances(fname)
+	n = len(d)
 
-    for k in range(n-2):
-        m = n - k
-        q, dict_q = calc_q(m, d)
-        i, j = dict_q[min(dict_q.keys())]
-        branchDist1, branchDist2 = calc_blen(m, i, j, d)
-        join(m, d, i, j)
-        print(k)
-        print(i, j)
-        import pprint
-        pprint.pprint(q)
-        pprint.pprint(d)
-    
+	children = {}
+	for k in range(n-1):
+		m = n - k
+		q, dict_q = calc_q(m, d)
+		i, j = dict_q[min(dict_q.keys())]
+
+		if k != n-2:
+			branchDist1, branchDist2 = calc_blen(m, i, j, d)
+			join(m, d, i, j)
+			children[n + k] = ((i, branchDist1), (j, branchDist2))
+		else:
+			children[n + k] = ((i, 0), (j, d[i][j]))
+
+
+	print(children)
+	
 def join(m, d, i, j):   
-    d += [[]]
-    n = len(d)
-    for k in range(n):
-        if(k == n-1):
-            d[n-1] += [0]
-            continue
-        d[n-1] += [0.5*(d[i][k] + d[j][k] - d[i][j])]
-        d[k] += [d[n-1][k]]
+	d += [[]]
+	n = len(d)
+	for k in range(n):
+		if(k == n-1):
+			d[n-1] += [0]
+			continue
+		d[n-1] += [0.5*(d[i][k] + d[j][k] - d[i][j])]
+		d[k] += [d[n-1][k]]
 
-    for k in range(n):
-        m = n - k
-        d[i][k] = INF
-        d[j][k] = INF
-        d[k][i] = INF
-        d[k][j] = INF
-    
+	for k in range(n):
+		m = n - k
+		d[i][k] = INF
+		d[j][k] = INF
+		d[k][i] = INF
+		d[k][j] = INF
+	
 
 def calc_blen(m, i, j, d):
-    n = len(d)
-    rows = sum_rows(d)
-    branchDist1 =  0.5 * d[i][j]+(1/(2*(m-2)))*(rows[i]-rows[j])
-    
-    branchDist2 =  0.5 * d[i][j]+(1/(2*(m-2)))*(rows[j]-rows[i])
+	n = len(d)
+	rows = sum_rows(d)
+	branchDist1 =  0.5 * d[i][j]+(1/(2*(m-2)))*(rows[i]-rows[j])
+	
+	branchDist2 =  0.5 * d[i][j]+(1/(2*(m-2)))*(rows[j]-rows[i])
 
-    return branchDist1, branchDist2
+	return branchDist1, branchDist2
 
 def sum_rows(d):
-    n = len(d)
-    rows = [0]*n
-    for i in range(len(d)):
-        for j in range(len(d)):
-            if(d[i][j] != INF):
-                rows[i] += d[i][j]
-    return rows
+	n = len(d)
+	rows = [0]*n
+	for i in range(len(d)):
+		for j in range(len(d)):
+			if(d[i][j] != INF):
+				rows[i] += d[i][j]
+	return rows
 
 def calc_q(m, d):
-    q = []
-    n = len(d)
-    q_dict = {}
+	q = []
+	n = len(d)
+	q_dict = {}
 
-    rows = sum_rows(d)
-    # print('rows:')
-    # print(rows)
+	rows = sum_rows(d)
 
-    for i in range(n):
-        q += [[]]
+	for i in range(n):
+		q += [[]]
 
-        for j in range(n):
-            if (i == j):
-                q[-1] += [0]
-                continue
-            val = (m-2) * (d[i][j]) - rows[i] - rows[j]
-            q[-1] += [val]
-            
-            q_dict[val] = (i,j)
-    
-    return q, q_dict
+		for j in range(n):
+			if (i == j):
+				q[-1] += [0]
+				continue
+			val = (m-2) * (d[i][j]) - rows[i] - rows[j]
+			if math.isnan(val):
+				val = float('inf')
+			q[-1] += [val]
+			
+			q_dict[val] = (i,j)
+	
+	return q, q_dict
 
 def get_distances(fname):
-    with open(fname) as f:
-        lines = f.readlines()
+	with open(fname) as f:
+		lines = f.readlines()
 
-        d = [[int(x) for x in line.strip().split()] for line in lines]
+		d = [[int(x) for x in line.strip().split()] for line in lines]
 
-    return d
+	return d
 
 if __name__ == '__main__':
-    fname = 'data.txt'
+	fname = sys.argv[1]
 
-    main(fname)
+	main(fname)
--- test.py
+++ test.py
@@ -4,11 +4,56 @@ MAXWEIGHT = 10
 NLEAVES = 4
 
 def main():
-    vertices, parents, children = gen_rand_tree()
-    get_distances(children[2*NLEAVES-2], children)
+	distance_matrix = get_empty_matrix(NLEAVES)
+	vertices, parents, children = gen_rand_tree()
+	get_distances(2*NLEAVES-2, children, distance_matrix)
 
-def get_distances(children):
+	for i in range(NLEAVES):
+		distance_matrix[i][i] = 0
+
+	with open('out.txt', 'w') as f:
+		for i in range(NLEAVES):
+			for j in range(NLEAVES):
+				f.write("%d " % distance_matrix[i][j])
+			f.write("\n")
+	print(distance_matrix)
+	print(parents)
+
+def get_empty_matrix(n):
+	distance_matrix = []
+
+	for i in range(n):
+		row= []
+	
+		for j in range(n):
+			row += [0]
+		
+		distance_matrix += [ row ]
     
+	return distance_matrix
+
+
+def get_distances(root,children, distance_matrix):
+	if root < NLEAVES:
+		return [root]
+	else:
+		left = get_distances(children[root][0][0], children, distance_matrix)
+		right = get_distances(children[root][1][0], children, distance_matrix)
+		weight_left = children[root][0][1]
+		weight_right= children[root][1][1]
+		
+		for child in left:
+			distance_matrix[child][child] += weight_left
+
+		for child in right:
+			distance_matrix[child][child] += weight_right
+
+		for left_child in left:
+			for right_child in right:
+				distance_matrix[left_child][right_child] = distance_matrix[left_child][left_child] + distance_matrix[right_child][right_child]
+				distance_matrix[right_child][left_child] = distance_matrix[left_child][right_child]
+
+		return left + right
 
 def gen_rand_tree():
     vertices = list(range(NLEAVES))
