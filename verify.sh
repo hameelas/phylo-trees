ee2cce599eb4cd6d3f62cadc65b7b8729e2eb2b2
Hamed Saleh <hameelas@gmail.com>
2019-05-13 20:42:49 -0400

    Added verify.sh

--- verify.sh
+++ verify.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 idx=0
-NVALUES="4 5 6 7 8 9 10 15 20 50 100 200 300"
-MAXWEIGHTS="10 100 1000 10000 1000000"
+NVALUES="4 5 6 7 8 9 10 15 20 50 100"
+MAXWEIGHTS="10 100 1000"
+
+white='\033[00m'
+red='\033[01;31m'
+green='\033[01;32m'
 
 for NLEAVES in ${NVALUES}; do
 	for MAXWEIGHT in ${MAXWEIGHTS}; do
 		idx=`expr ${idx} + 1`
 		echo -ne "[${idx}]\tgen "
 		python3 ./test.py gen "tests/${idx}.txt" "${NLEAVES}" "${MAXWEIGHT}" >> log.txt
-		sleep 1
-		echo -ne "[OK] check "
-		python3 ./test.py check "tests/${idx}.txt" >> log.txt
+		echo -ne "${green}[OK]${white} check "
+		python3 ./test.py check "tests/${idx}.txt" "${NLEAVES}" "${MAXWEIGHT}" >> log.txt
 		ret="$?"
-		echo -ne "ret = ${ret} "
-		[ "${ret}" == 0 ] && echo -ne "[OK]" || echo -ne "[NO]"
+		[ "${ret}" == 0 ] && echo -ne "${green}[OK]${white}" || echo -ne "${red}[NO]${white}"
 		echo ""
 	done
 done
